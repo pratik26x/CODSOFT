975b732af949520cdc40a1a8a9b6354697396f2f
Pratik Rakhonde <pratiksrakhonde@gmail.com>
2023-08-12 23:49:53 +0530

    first commit

---
+++ java/forloop1.java
@@ -0,0 +1,12 @@
+// print hello world 10 times using for loop
+public class forloop1 
+{
+    public static void main(String args[])
+    {
+        for( int i=1; i<=10; i++ )
+            {
+            System.out.println("Hello World");
+            }    
+    }
+
+}
---
+++ java/forloop2.java
@@ -0,0 +1,11 @@
+// print square pattern using forloop
+public class forloop2
+{
+    public static void main(String args[])
+    {
+        for (int line = 1; line <= 4; line++)
+        {
+            System.out.println( " * * * * ");
+        }
+    }
+}
